c01a9d00b22bd2d1d227c599f2f243b7af7e526f
KarthikMohan7639 <karthikmohan7639@gmail.com>
2025-08-30 21:00:45 +0530

    staged changes success db connection

--- src/dbConfig/mongoConnect.ts
+++ src/dbConfig/mongoConnect.ts
@@ -1,37 +1,27 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri = process.env.DATABASE_URL!;
+if (!uri) throw new Error('DATABASE_URL not set');
 
-if (!uri) {
-  throw new Error('DATABASE_URL environment variable is not set');
-}
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV !== 'production') {
-  // In development, use a global variable so the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
+    client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  // In production, it's best to not use a global variable.
-  client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
